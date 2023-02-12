cec6c11169b9b1a40d8ca85ec89eb5fb36bd903f
congminh2812 <congminh281297@gmail.com>
2023-02-12 23:40:39 +0700

    add project

---
+++ AppMVC/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AppMVC.Data;
+using AppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> listCategory = _db.Categories;
+            return View(listCategory);
+        }
+    }
+}
---
+++ AppMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AppMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { } 
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
