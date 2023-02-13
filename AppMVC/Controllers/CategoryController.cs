02217daed4bc3f38eaa3ea6c9d5037f0ab7f0001
congminh2812 <congminh281297@gmail.com>
2023-02-13 22:44:15 +0700

    update delete

--- AppMVC/Controllers/CategoryController.cs
+++ AppMVC/Controllers/CategoryController.cs
@@ -35,10 +35,71 @@ namespace AppMVC.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Create category successfully!";
                 return RedirectToAction("Index");
             }
 
             return View(category);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is (null or 0))
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category is null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "The DisplayOrder cannot match with Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"]="Update category successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is (null or 0))
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category is null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var category = _db.Categories.Find(id);
+
+            if (category is null) return NotFound();
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"]="Delete category successfully!";
+
+            return RedirectToAction("Index");
         }
     }
 }
