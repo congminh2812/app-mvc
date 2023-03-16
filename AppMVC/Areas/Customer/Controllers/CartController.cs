89bfd4ff66157675768dbc99ff729844483f23b5
congminh2812 <congminh281297@gmail.com>
2023-03-16 16:27:54 +0700

    add summary

--- AppMVC/Areas/Customer/Controllers/CartController.cs
+++ AppMVC/Areas/Customer/Controllers/CartController.cs
@@ -33,7 +33,68 @@ namespace AppMVC.Web.Areas.Customer.Controllers
             foreach (var item in ShoppingCartVM.ListCart)
                 item.Price = GetPriceBasedOnQuantity(item);
 
+            ShoppingCartVM.CartTotal = ShoppingCartVM.ListCart.Sum(s => s.Price * s.Count);
+
             return View(ShoppingCartVM);
+        }
+
+        public IActionResult Summary()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity!;
+            //var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //ShoppingCartVM = new ShoppingCartVM
+            //{
+            //    ListCart = _unitOfWork.ShoppingCart.GetAll(s => s.ApplicationUserId == claims!.Value, includeProperties: "Product")
+            //};
+
+            //foreach (var item in ShoppingCartVM.ListCart)
+            //    item.Price = GetPriceBasedOnQuantity(item);
+
+            //ShoppingCartVM.CartTotal = ShoppingCartVM.ListCart.Sum(s => s.Price * s.Count);
+
+            //return View(ShoppingCartVM);
+
+            return View();
+        }
+
+        public IActionResult Plus(int cartId) { 
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+
+            if (cart is not null)
+            {
+                _unitOfWork.ShoppingCart.Increasement(cart, 1);
+                _unitOfWork.Save();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+
+            if (cart.Count - 1 <= 0)
+                _unitOfWork.ShoppingCart.Remove(cart);
+            else 
+                _unitOfWork.ShoppingCart.Decreasement(cart, 1);
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+
+            if (cart is not null)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
